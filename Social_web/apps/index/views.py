1098d2bee5b44a918c2078069e9e6afcf80cba5c
qike <18309610151@163.com>
2019-12-10 16:10:40 +0800

    'feat:展示主页'

--- Social_web/apps/index/views.py
+++ Social_web/apps/index/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import View
 
+
+class IndexView(View):
+    def get(self,request):
+        """
+        主页
+        :param request:
+        :return:
+        """
+        return render(request,'index.html')
 # Create your views here.
